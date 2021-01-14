60c2cd163985b85c864ac804bcb92454bd82799c
Ingrid <ingridsilvatec@outlook.com>
2021-01-14 12:17:16 -0300

    implementa layout landscape

--- Orientacao/app/src/main/java/com/sinngjpeg/orientacao/MainActivity.kt
+++ Orientacao/app/src/main/java/com/sinngjpeg/orientacao/MainActivity.kt
@@ -2,10 +2,36 @@ package com.sinngjpeg.orientacao
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import com.sinngjpeg.orientacao.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    var names = arrayListOf<String>()
+    var adapter: ArrayAdapter<String>? = null
+    val list = android.R.layout.simple_list_item_1
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        val binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        if (savedInstanceState != null) {
+            names = savedInstanceState.getStringArrayList("names_list") ?: arrayListOf()
+        }
+        adapter = ArrayAdapter(this, list, names)
+        binding.lstNames.adapter = adapter
+    }
+
+    fun btnAddClick(view: View) {
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        names.add(binding.edtName.text.toString())
+        binding.edtName.text.clear()
+        adapter?.notifyDataSetChanged()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState?.putStringArrayList("names_list", names)
     }
 }
