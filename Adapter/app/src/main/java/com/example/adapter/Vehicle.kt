c604361edc3814bc17d58cd6eb9a1db1d67b7589
Ingrid Silva <ingridsilvatec@outlook.com>
2021-01-21 16:15:12 -0300

    implementa adapter, main, strings, arrays

--- Adapter/app/src/main/java/com/example/adapter/Vehicle.kt
+++ Adapter/app/src/main/java/com/example/adapter/Vehicle.kt
@@ -5,7 +5,7 @@ import java.util.*
 data class Vehicle(
     var model: String,
     var year: Int,
-    var manufacturer: Int, //o = VW; 1 = GM; 2 = Fiat; 3 = Ford
+    var manufacturer: Int, //0 = VW; 1 = GM; 2 = Fiat; 3 = Ford
     var gasoline: Boolean,
     var ethanol: Boolean
 )
