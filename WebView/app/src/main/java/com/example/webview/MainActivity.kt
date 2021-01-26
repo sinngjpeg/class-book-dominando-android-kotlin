608011b0b39b8c281cc4e271678aabf07c131275
Ingrid Silva <ingridsilvatec@outlook.com>
2021-01-26 18:43:11 -0300

    implementa page.html

--- WebView/app/src/main/java/com/example/webview/MainActivity.kt
+++ WebView/app/src/main/java/com/example/webview/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.webview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.JavascriptInterface
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val settings = webView.settings
+        settings.javaScriptEnabled = true
+        webView.addJavascriptInterface(this, "dominando")
+        webView.loadUrl("file:///android_assets/app_page.html")
+    }
+
+    @JavascriptInterface
+    fun showToast(s: String, t: String) {
+        Toast.makeText(this, "Nome: $s Idade: $t", Toast.LENGTH_SHORT).show();
     }
 }
