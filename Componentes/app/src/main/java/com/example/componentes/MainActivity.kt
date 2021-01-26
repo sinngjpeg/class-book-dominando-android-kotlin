fe1912ae4c95bcddd6618139f28c54db7c779b25
Ingrid Silva <ingridsilvatec@outlook.com>
2021-01-26 15:38:17 -0300

    implementa activity

--- Componentes/app/src/main/java/com/example/componentes/MainActivity.kt
+++ Componentes/app/src/main/java/com/example/componentes/MainActivity.kt
@@ -6,64 +6,59 @@ import android.widget.ArrayAdapter
 import android.widget.RadioButton
 import android.widget.SeekBar
 import android.widget.Toast
-import com.example.componentes.databinding.ActivityMainBinding
+
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    val binding = ActivityMainBinding.inflate(layoutInflater)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
         initSpinner()
         initSeekbar()
         initSwitch()
         // Atribuindo programaticamente os valores-padrão
-        binding.chkEnabled.isChecked = true
-        binding.skbValue.progress = 20
-        binding.spnNames.setSelection(2)
-        binding.rgOptions.check(R.id.rbOption2)
-        binding.btnShowValues.setOnClickListener { showValues() }
+        chkEnabled.isChecked = true
+        skbValue.progress = 20
+        spnNames.setSelection(2)
+        rgOptions.check(R.id.rbOption2)
+        btnShowValues.setOnClickListener { showValues() }
     }
-
     private fun initSpinner() {
         val names = arrayOf("Eric", "Diana", "Presto", "Hank", "Sheila", "Bob")
         val adapter = ArrayAdapter(
-                this, android.R.layout.simple_spinner_item, names)
+            this, android.R.layout.simple_spinner_item, names)
         adapter.setDropDownViewResource(
-                android.R.layout.simple_spinner_dropdown_item)
-        binding.spnNames.adapter = adapter
+            android.R.layout.simple_spinner_dropdown_item)
+        spnNames.adapter = adapter
     }
-
     private fun initSeekbar() {
-        binding.skbValue.setOnSeekBarChangeListener(
-                object : SeekBar.OnSeekBarChangeListener {
-                    override fun onProgressChanged(seekBar: SeekBar, i: Int, b: Boolean) {
-                        binding.txtValue.text = i.toString()
-                    }
-
-                    override fun onStartTrackingTouch(seekBar: SeekBar) {}
-                    override fun onStopTrackingTouch(seekBar: SeekBar) {}
-                })
+        skbValue.setOnSeekBarChangeListener(
+            object : SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(seekBar: SeekBar, i: Int, b: Boolean) {
+                    txtValue.text = i.toString()
+                }
+                override fun onStartTrackingTouch(seekBar: SeekBar) {}
+                override fun onStopTrackingTouch(seekBar: SeekBar) {}
+            })
     }
-
     private fun initSwitch() {
-        binding.swtEnabled.setOnCheckedChangeListener { compoundButton, b ->
-            binding.chkEnabled.isEnabled = compoundButton.isChecked
-            binding.tgbEnabled.isEnabled = b
+        swtEnabled.setOnCheckedChangeListener { compoundButton, b ->
+            chkEnabled.isEnabled = compoundButton.isChecked
+            tgbEnabled.isEnabled = b
         }
     }
-
     private fun showValues() {
-        val idSelectedRadio = binding.rgOptions.checkedRadioButtonId
+        val idSelectedRadio = rgOptions.checkedRadioButtonId
         val radio = findViewById<RadioButton>(idSelectedRadio)
         val enabledText = getString(
-                if (binding.chkEnabled.isChecked) R.string.text_enabled
-                else R.string.text_disabled
+            if (chkEnabled.isChecked) R.string.text_enabled
+            else R.string.text_disabled
         )
         val message = getString(R.string.msg_result,
-                enabledText,
-                binding.skbValue.progress,
-                binding.spnNames.selectedItem,
-                radio.text.toString()
+            enabledText,
+            skbValue.progress,
+            spnNames.selectedItem,
+            radio.text.toString()
         )
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
