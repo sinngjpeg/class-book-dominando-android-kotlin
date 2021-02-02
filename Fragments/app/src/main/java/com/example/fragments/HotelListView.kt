2639348f17f1a124baaacf2103adf6d04fadccd7
Ingrid Silva <ingridsilvatec@outlook.com>
2021-02-02 18:00:47 -0300

    create HotelListPresenter

--- Fragments/app/src/main/java/com/example/fragments/HotelListView.kt
+++ Fragments/app/src/main/java/com/example/fragments/HotelListView.kt
@@ -1,4 +1,8 @@
 package com.example.fragments
 
+import com.example.fragments.data.Hotel
+
 interface HotelListView {
+    fun showHotels(hotels: List<Hotel>)
+    fun showHotelsDetails(hotel: Hotel)
 }
