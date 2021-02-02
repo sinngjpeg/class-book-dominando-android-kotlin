b495e07c1cb43e885db8f8483995347e8933276b
Ingrid Silva <ingridsilvatec@outlook.com>
2021-02-02 17:46:27 -0300

    create memorory repository, interface ListView

--- Fragments/app/src/main/java/com/example/fragments/HotelRepository.kt
+++ Fragments/app/src/main/java/com/example/fragments/HotelRepository.kt
@@ -1,4 +1,10 @@
 package com.example.fragments
 
+import com.example.fragments.data.Hotel
+
 interface HotelRepository {
+    fun save(hotel: Hotel)
+    fun remove(vararg hotels: Hotel)
+    fun hotelById(id: Long, callback: (Hotel?) -> Unit)
+    fun search(term: String, callback: (List<Hotel>?) -> Unit)
 }
