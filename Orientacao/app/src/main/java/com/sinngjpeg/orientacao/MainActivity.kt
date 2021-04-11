e3489ae4b835d35fe54e9f4c9dcdeef1cf3ab849
Ingrid Silva <ingridsilvatec@outlook.com>
2021-04-10 22:20:01 -0300

    implementa saveInstanceState, para orientação da tela

--- Orientacao/app/src/main/java/com/sinngjpeg/orientacao/MainActivity.kt
+++ Orientacao/app/src/main/java/com/sinngjpeg/orientacao/MainActivity.kt
@@ -4,27 +4,30 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ArrayAdapter
+import android.widget.ListView
 import com.sinngjpeg.orientacao.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
     var names = arrayListOf<String>()
     var adapter: ArrayAdapter<String>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        val list = android.R.layout.simple_list_item_1
-        val binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         if (savedInstanceState != null) {
             names = savedInstanceState.getStringArrayList("names_list") ?: arrayListOf()
         }
-        adapter = ArrayAdapter(this, list, names)
-        binding.lstNames.adapter = adapter
+
+        var adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, names)
+        var lstNames = binding.lstNames
+        lstNames.adapter = adapter
     }
 
     fun btnAddClick(view: View) {
-        val binding = ActivityMainBinding.inflate(layoutInflater)
         names.add(binding.edtName.text.toString())
         binding.edtName.text.clear()
         adapter?.notifyDataSetChanged()
@@ -32,6 +35,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState?.putStringArrayList("names_list", names)
+        outState.putStringArrayList("names_list", names)
     }
 }
